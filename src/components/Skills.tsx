91113a3adeef5cb04716eaf4b9f8e1325ff6dad4
AffanDj <affan.djafar@gmail.com>
2026-03-12 15:14:54 +0700

    fix color card

--- src/components/Skills.tsx
+++ src/components/Skills.tsx
@@ -1,70 +1,70 @@
 import Image from 'next/image';
 
 const skills = [
-  { 
-    name: 'ReactJS', 
+  {
+    name: 'ReactJS',
     color: 'bg-fuchsia-300 dark:bg-fuchsia-600',
-    icon: '/icons/react.svg' 
+    icon: '/icons/react.svg'
   },
-  { 
-    name: 'React Native', 
+  {
+    name: 'React Native',
     color: 'bg-violet-400 dark:bg-violet-600',
-    icon: '/icons/react.svg' 
+    icon: '/icons/react.svg'
   },
-  { 
-    name: 'Next.js', 
+  {
+    name: 'Next.js',
     color: 'bg-cyan-300 dark:bg-cyan-600',
-    icon: '/icons/nextdotjs.svg' 
+    icon: '/icons/nextdotjs.svg'
   },
-  { 
-    name: 'TypeScript', 
+  {
+    name: 'TypeScript',
     color: 'bg-blue-400 dark:bg-blue-600',
-    icon: '/icons/typescript.svg' 
+    icon: '/icons/typescript.svg'
   },
-  { 
-    name: 'Node.js', 
+  {
+    name: 'Node.js',
     color: 'bg-green-300 dark:bg-green-600',
-    icon: '/icons/nodedotjs.svg' 
+    icon: '/icons/nodedotjs.svg'
   },
-  { 
-    name: 'C++', 
+  {
+    name: 'C++',
     color: 'bg-blue-500 dark:bg-blue-700',
-    icon: '/icons/cplusplus.svg' 
+    icon: '/icons/cplusplus.svg'
   },
-  { 
-    name: 'Java', 
+  {
+    name: 'Java',
     color: 'bg-red-500 dark:bg-red-700',
-    icon: '/icons/openjdk.svg' 
+    icon: '/icons/openjdk.svg'
   },
-  { 
-    name: 'HTML5', 
+  {
+    name: 'HTML5',
     color: 'bg-orange-400 dark:bg-orange-600',
-    icon: '/icons/html5.svg' 
+    icon: '/icons/html5.svg'
   },
-  { 
-    name: 'CSS3', 
+  {
+    name: 'CSS3',
     color: 'bg-sky-500 dark:bg-sky-700',
-    icon: '/icons/css.svg' 
+    icon: '/icons/css.svg'
   },
-  { 
-    name: 'Tailwind', 
+  {
+    name: 'Tailwind',
     color: 'bg-yellow-300 dark:bg-yellow-500',
-    icon: '/icons/tailwindcss.svg' 
+    icon: '/icons/tailwindcss.svg'
   },
-  { 
-    name: 'MySQL', 
+  {
+    name: 'MySQL',
     color: 'bg-orange-300 dark:bg-orange-500',
-    icon: '/icons/mysql.svg' 
+    icon: '/icons/mysql.svg'
   },
-  { 
-    name: 'Figma', 
+  {
+    name: 'Figma',
     color: 'bg-indigo-300 dark:bg-indigo-500',
-    icon: '/icons/figma.svg' 
+    icon: '/icons/figma.svg'
   },
-  { 
-    name: 'GitBash', 
+  {
+    name: 'GitBash',
     color: 'bg-slate-400 dark:bg-slate-600',
-    icon: '/icons/git.svg' 
+    icon: '/icons/git.svg'
   },
   {
     name: 'Jest',
@@ -78,7 +78,7 @@ const skills = [
   },
   {
     name: 'Redux',
-    color: 'bg-rose-300 dark:bg-rose-700',
+    color: 'bg-zinc-300 dark:bg-zinc-700',
     icon: '/icons/redux.svg'
   }
 ];
@@ -91,22 +91,22 @@ export default function Skills() {
       </h2>
       <div className="grid grid-cols-2 md:grid-cols-4 gap-8 max-w-5xl mx-auto">
         {skills.map((skill, index) => (
-          <div 
+          <div
             key={skill.name}
             className={`pixel-border ${skill.color} p-6 flex flex-col items-center hover:-translate-y-1 transition-transform`}
             data-aos="zoom-in"
             data-aos-delay={index * 50}
           >
             <div className="w-12 h-12 bg-white dark:bg-slate-900 border-4 border-black dark:border-white mb-4 flex items-center justify-center p-2 relative">
-               <Image 
-                  src={skill.icon} 
-                  alt={skill.name}
-                  width={24}
-                  height={24}
-                  className="dark:invert transition-all duration-300"
-               />
+              <Image
+                src={skill.icon}
+                alt={skill.name}
+                width={24}
+                height={24}
+                className="dark:invert transition-all duration-300"
+              />
             </div>
-            
+
             <h3 className="text-[10px] text-black dark:text-white text-center leading-relaxed font-bold">
               {skill.name}
             </h3>
